18df05769dfdee2e4b41e30252fa94a288fd04aa
personal <hasinmahmood01@gmail.com>
2024-04-04 15:28:29 +0100

    chore: test ip

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import React from "react";
 import Carousel from "./modules/carousel/carousel";
 import JournalDescription from "./modules/description/description";
+import { headers } from "next/headers";
+
+export function getIp() {
+  let forwardedFor = headers().get("x-forwarded-for");
+  let realIp = headers().get("x-real-ip");
+  if (forwardedFor) {
+    return forwardedFor.split(", ")[0].trim();
+  }
+  if (realIp) return realIp.trim();
+  return null; // or '0.0.0.0', depends
+}
 
 export default function Home() {
   return (
@@ -10,6 +21,7 @@ export default function Home() {
         <Carousel />
         <JournalDescription />
       </div>
+      {getIp()}
     </main>
   );
 }

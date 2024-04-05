d202750a9b1248ab4555ccfe63bc670a5be65133
personal <hasinmahmood01@gmail.com>
2024-04-05 17:25:12 +0100

    chore:change image format to webp for faster loading

--- app/modules/description/description.tsx
+++ app/modules/description/description.tsx
@@ -3,16 +3,18 @@ import BuyWithAmazonBtn from "../buy-with-amazon-btn/buy-with-amazon-btn";
 
 const JournalDescription = () => {
   return (
-    <div className="md:w-2/6 flex flex-col gap-4">
-      <h2 className="font-sans text-xl">Hard Cover Journal</h2>
-      <p className="font-sans">
-        Embark on a transformative journey with &quot;The Limitless
-        Project&quot; – a journal meticulously crafted to guide you through a
-        91-day path of self-improvement. With each day structured to foster
-        growth, this journal is your daily companion towards a disciplined,
-        focused, and fulfilling life.
-      </p>
-      <JournalContentInfo />
+    <div className="md:w-2/6 flex flex-col justify-between">
+      <div className="flex flex-col gap-4">
+        <h2 className="font-sans text-xl">Hard Cover Journal</h2>
+        <p className="font-sans">
+          Embark on a transformative journey with &quot;The Limitless
+          Project&quot; – a journal meticulously crafted to guide you through a
+          91-day path of self-improvement. With each day structured to foster
+          growth, this journal is your daily companion towards a disciplined,
+          focused, and fulfilling life.
+        </p>
+        <JournalContentInfo />
+      </div>
       <BuyWithAmazonBtn />
     </div>
   );

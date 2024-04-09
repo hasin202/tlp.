13c1d33562b336b2002370f476d5e0c81125f9ed
personal <hasinmahmood01@gmail.com>
2024-04-09 18:53:48 +0100

    feat: add footer

--- app/modules/page-break/index.tsx
+++ app/modules/page-break/index.tsx
@@ -1,7 +1,7 @@
 const PageBreak = () => {
   return (
     <div className="inline-flex items-center justify-center w-full">
-      <hr className=" h-px my-8 bg-gray-600 border-0 w-full" />
+      <hr className="h-px my-8 bg-gray-600 border-0 w-full" />
       <span className="absolute px-3 -translate-x-1/2 bg-black left-1/2 font-galderglynn text-2xl">
         TLP.
       </span>

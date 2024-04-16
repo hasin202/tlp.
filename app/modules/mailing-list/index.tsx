f718ec2f0a774b3abac29f74147f5c6890ebeb58
personal <hasinmahmood01@gmail.com>
2024-04-16 18:10:50 +0100

    feat: add mailing list to new layout

--- app/modules/mailing-list/index.tsx
+++ app/modules/mailing-list/index.tsx
@@ -2,9 +2,9 @@ import MailInput from "./input";
 
 const MailingList = () => {
   return (
-    <div className="flex flex-col gap-8 items-center justify-between w-full">
-      <h1 className="text-2xl font-bold">Get a Free Gift!</h1>
-      <p className="w-[24rem] text-center">
+    <div className="flex flex-col gap-8 items-center justify-between w-full px-4">
+      <h1 className="text-2xl font-bold">Want a Free Wallpaper Pack?</h1>
+      <p className="w-full md:w-[24rem] text-center">
         Sign up to the limitless mailing list for deals, updates and improvement
         insights.
       </p>

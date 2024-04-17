ab64a1b6813d8ea5b95bf977de513db9713539a5
personal <hasinmahmood01@gmail.com>
2024-04-17 20:05:14 +0100

    chore:use points from seperate file

--- app/modules/extra-info/weeky-reviews/index.tsx
+++ app/modules/extra-info/weeky-reviews/index.tsx
@@ -6,25 +6,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-
-const points = [
-  {
-    title: "Reflect on the last 7 days",
-    info: "Engage in deep reflection with a review that helps align your actions with your goals. What did you spend the most time on in the last week? Was this really what should have had your attention? How can next week be better?",
-  },
-  {
-    title: "Plan for the next 7 days",
-    info: "Set intentions and plan actionable steps for the week ahead.",
-  },
-  {
-    title: "Track habits for Success",
-    info: "Track and build up to three key habits, reinforcing the discipline essential for success.",
-  },
-  {
-    title: "Do some General Journalling",
-    info: "Now, this is a space where you can dump all of your feelings and thoughts for the previous week. Maybe you just had a bad day and want to vent. Write it here. Maybe you achieved something you're proud of. Write it here. Maybe you saw something amazing. Write it here. You get the point. Just write anything and everything about the last week here. ",
-  },
-];
+import points from "./points";
 
 const WeeklyReviews = () => {
   return (

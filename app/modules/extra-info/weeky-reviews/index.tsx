8471ed807f28b5ba1de5bf8d8a3f78b427f0ca57
personal <hasinmahmood01@gmail.com>
2024-04-17 20:03:40 +0100

    feat:implement accordion for extra info about weekly reviews

--- app/modules/extra-info/weeky-reviews/index.tsx
+++ app/modules/extra-info/weeky-reviews/index.tsx
@@ -1,30 +1,53 @@
 import ConfigurableCTA from "../../call-to-actions/configurable-cta";
 
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 const points = [
-  "Engage in deep reflection with a review that helps align your actions with your goals.",
-  "Set intentions and plan actionable steps for the week ahead.",
-  "Track and build up to three key habits, reinforcing the discipline essential for success.",
+  {
+    title: "Reflect on the last 7 days",
+    info: "Engage in deep reflection with a review that helps align your actions with your goals. What did you spend the most time on in the last week? Was this really what should have had your attention? How can next week be better?",
+  },
+  {
+    title: "Plan for the next 7 days",
+    info: "Set intentions and plan actionable steps for the week ahead.",
+  },
+  {
+    title: "Track habits for Success",
+    info: "Track and build up to three key habits, reinforcing the discipline essential for success.",
+  },
+  {
+    title: "Do some General Journalling",
+    info: "Now, this is a space where you can dump all of your feelings and thoughts for the previous week. Maybe you just had a bad day and want to vent. Write it here. Maybe you achieved something you're proud of. Write it here. Maybe you saw something amazing. Write it here. You get the point. Just write anything and everything about the last week here. ",
+  },
 ];
 
 const WeeklyReviews = () => {
   return (
     <div className="flex flex-col gap-8 md:flex-row-reverse md:justify-end md:gap-16 md:items-center">
       <div className="flex flex-col gap-4 md:gap-8">
-        <h2 className="font-galderglynn text-3xl md:text-5xl">
-          Goal setting guidance
-        </h2>
-        <ul className="flex flex-col gap-2">
-          {points.map((goal, key) => {
+        <div>
+          <h2 className="font-galderglynn text-3xl md:text-5xl">
+            Weekly Reviews
+          </h2>
+          <h4 className="font-light text-2xl text-gray-500 uppercase">
+            To keep you focused
+          </h4>
+        </div>
+        <Accordion type="single" collapsible className="w-full">
+          {points.map((point, key) => {
             return (
-              <li
-                className="relative before:content-['+'] before:absolute before:left-0 before:top-0"
-                key={key}
-              >
-                <p className="ml-6">{goal}</p>
-              </li>
+              <AccordionItem value={`point-${key}`}>
+                <AccordionTrigger>{point.title}</AccordionTrigger>
+                <AccordionContent>{point.info}</AccordionContent>
+              </AccordionItem>
             );
           })}
-        </ul>
+        </Accordion>
         <ConfigurableCTA ctaText="Set yourself up for Success" />
       </div>
       <img src="/assets/review.png" className="md:w-[50%]" />

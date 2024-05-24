06362ada889eac191eb08eb8b3ebb7f69adbc7c4
personal <hasinmahmood01@gmail.com>
2024-05-24 18:02:55 +0100

    fix: update the ux and logic for getting users a dynamic amazon url

--- app/modules/call-to-actions/index.tsx
+++ app/modules/call-to-actions/index.tsx
@@ -1,17 +1,18 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
-import useCountryBasedUrl from "./use-country-based-url";
+import React from "react";
 
-const BuyWithAmazonBtn = () => {
-  const url = useCountryBasedUrl();
+type TProps = {
+  link: string | null;
+};
 
+const BuyWithAmazonBtn: React.FC<TProps> = async ({ link }) => {
+  if (!link) return;
   return (
     <Button className="w-40 md:w-44 2xl:w-64">
       <a
         className="text-sm flex items-center justify-center gap-2 md:gap-4 w-full py-4"
         target="_blank"
-        href={url}
+        href={link}
       >
         <img src="/assets/amazon.svg" className="w-4 md:w-6" />
         <p>Buy Now</p>
--- app/modules/call-to-actions/use-country-based-url.ts
+++ app/modules/call-to-actions/use-country-based-url.ts
@@ -1,15 +1,5 @@
-import { useEffect, useState } from "react";
-
-const getGeoInfo = async (): Promise<string> => {
-  try {
-    const response = await fetch("https://ipapi.co/json/");
-    const data = await response.json();
-    return data.country || "US";
-  } catch (error) {
-    console.error("Failed to fetch geo info:", error);
-    return "US"; // Default to US in case of error
-  }
-};
+import { headers } from "next/headers";
+import { TGeoApiRes } from "./type";
 
 const amazonUrls: { [key: string]: string } = {
   US: "https://www.amazon.com/Limitless-Project-Unlock-Your-Potential/dp/B0CVGVSVPV/ref=sr_1_1?crid=37EE7VBRHG2RG&dib=eyJ2IjoiMSJ9.9GIXRLLkhTA4jEGVUSl_C4PVzXg67BloQ17eh2LZATf6ntC-yDZtOHn-B4tUT0v3xlMwl_GMcY-nfB2RcFC-7LnWG3ul_ILzNJ4OF1IMehwC0hZSxs-ZupHJsl6Ola-yIFy-f0Gc6uWusD24YYO40RVj4YXBNz12CdZ2capRej4ij5nygbqz-8yki8RY2msS1K8ffGLgRZi8rG6CxWDx70ZvquBzfaRWajXGxG5TvxU.ipLeNwBZSUlpAHyRb0pX4ads2oFggsYK_JYPsliaf3U&dib_tag=se&keywords=the+limitless+project&qid=1712180904&sprefix=the+limitless+pr%2Caps%2C95&sr=8-1",
@@ -26,18 +16,20 @@ const amazonUrls: { [key: string]: string } = {
   AU: "https://www.amazon.com.au/Limitless-Project-Unlock-Your-Potential/dp/B0CZM6X6LY/ref=tmm_pap_swatch_0?_encoding=UTF8&dib_tag=se&dib=eyJ2IjoiMSJ9.9GIXRLLkhTA4jEGVUSl_C4PVzXg67BloQ17eh2LZATf6ntC-yDZtOHn-B4tUT0v3xlMwl_GMcY-nfB2RcFC-7LnWG3ul_ILzNJ4OF1IMehwC0hZSxs-ZupHJsl6Ola-yIFy-f0Gc6uWusD24YYO40RVj4YXBNz12CdZ2capRej4ij5nygbqz-8yki8RY2msS1K8ffGLgRZi8rG6CxWDx70ZvquBzfaRWajXGxG5TvxU.ipLeNwBZSUlpAHyRb0pX4ads2oFggsYK_JYPsliaf3U&qid=1712180904&sr=8-1",
 };
 
-export default function useCountryBasedUrl() {
-  const [url, setUrl] = useState(
-    "https://www.amazon.com/Limitless-Project-Unlock-Your-Potential/dp/B0CVGVSVPV/ref=sr_1_1?crid=37EE7VBRHG2RG&dib=eyJ2IjoiMSJ9.9GIXRLLkhTA4jEGVUSl_C4PVzXg67BloQ17eh2LZATf6ntC-yDZtOHn-B4tUT0v3xlMwl_GMcY-nfB2RcFC-7LnWG3ul_ILzNJ4OF1IMehwC0hZSxs-ZupHJsl6Ola-yIFy-f0Gc6uWusD24YYO40RVj4YXBNz12CdZ2capRej4ij5nygbqz-8yki8RY2msS1K8ffGLgRZi8rG6CxWDx70ZvquBzfaRWajXGxG5TvxU.ipLeNwBZSUlpAHyRb0pX4ads2oFggsYK_JYPsliaf3U&dib_tag=se&keywords=the+limitless+project&qid=1712180904&sprefix=the+limitless+pr%2Caps%2C95&sr=8-1"
-  );
+const getCountryBasedUrl = async (): Promise<string | null> => {
+  const headersList = headers();
+  const clientIp = headersList.get("x-forwarded-for");
+  let countryCode;
+  try {
+    // Make a request to the geolocation API
+    const res = await fetch(`http://ip-api.com/json/${clientIp}`);
+    const data: TGeoApiRes = await res.json();
+    countryCode = data.countryCode;
+    return amazonUrls[countryCode] || null;
+  } catch (error) {
+    console.error("Geolocation API error:", error);
+    return null;
+  }
+};
 
-  useEffect(() => {
-    const setCode = async () => {
-      let code = await getGeoInfo();
-      setUrl(amazonUrls[code] || amazonUrls["US"]);
-    };
-    setCode();
-  }, []);
-
-  return url;
-}
+export default getCountryBasedUrl;
